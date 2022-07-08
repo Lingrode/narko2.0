86f623d39ab86aed1f74d5389aea54329cf369d2
Miron <kolomiietsnazar@gmail.com>
2022-07-08 21:52:06 +0300

    js

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,23 +1,34 @@
 // Custom Scripts
 // Custom scripts
-$(function () {
-	$('.header-nav__list-item').hover(
-		function () {
-			$(this).children('div').addClass('visible');
-		}, function () {
-			$(this).children('div').removeClass('visible');
-		}
-	);
+// $(function () {
+//   $(".header-nav__list-item").hover(
+//     function () {
+//       $(this).children("div").addClass("visible");
+//     },
+//     function () {
+//       $(this).children("div").removeClass("visible");
+//     }
+//   );
+//   let menuItem = $(".header-nav__list-item");
+//   menuItem.hover(
+//     function () {
+//       $(this).addClass("hovered");
+//     },
+//     function () {
+//       $(this).removeClass("hovered");
+//     }
+//   );
+// });
 
-	//let menuLink = $('.header-nav__list-link');
-	let menuItem = $('.header-nav__list-item');
-	// menuItem.each(function() {
-	menuItem.hover(
-		function () {
-			$(this).addClass('hovered');
-		},
-		function () {
-			$(this).removeClass('hovered');
-		}
-	)
-});
+const vis = document.querySelectorAll(".header-nav__list-link");
+const drop = document.querySelectorAll(".dropdown-menu");
+
+for (let i = 0; i < vis.length; i++) {
+  vis[i].addEventListener("mouseover", () => {
+    drop[i].classList.add("visible");
+  });
+  vis[i].addEventListener("mouseout", () => {
+    drop[i].classList.remove("visible");
+  });
+}
+
