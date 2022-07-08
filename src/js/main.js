375e19453d971f144d4e030a945bcff202ba005e
Illia <grimaloilya@gmail.com>
2022-07-08 21:55:22 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
 // Custom scripts
 $(function () {
-	$('.header-nav__list-item').hover(
-		function () {
-			$(this).children('div').addClass('visible');
-		}, function () {
-			$(this).children('div').removeClass('visible');
-		}
-	);
+  $('.header-nav__list-item').hover(
+    function () {
+      $(this).children('div').addClass('visible');
+    }, function () {
+      $(this).children('div').removeClass('visible');
+    }
+  );
 
-	//let menuLink = $('.header-nav__list-link');
-	let menuItem = $('.header-nav__list-item');
-	// menuItem.each(function() {
-	menuItem.hover(
-		function () {
-			$(this).addClass('hovered');
-		},
-		function () {
-			$(this).removeClass('hovered');
-		}
-	)
+  //let menuLink = $('.header-nav__list-link');
+  let menuItem = $('.header-nav__list-item');
+  // menuItem.each(function() {
+  menuItem.hover(
+    function () {
+      $(this).addClass('hovered');
+    },
+    function () {
+      $(this).removeClass('hovered');
+    }
+  )
 });
