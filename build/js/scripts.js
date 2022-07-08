0b43a28cfc9ad330dd2ca6e994dd6a8fae7664c5
Miron <kolomiietsnazar@gmail.com>
2022-07-08 22:35:27 +0300

    update fonts

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,5 +1,6 @@
 // Custom Scripts
 // Custom scripts
+
 // $(function () {
 //   $(".header-nav__list-item").hover(
 //     function () {
@@ -20,15 +21,17 @@
 //   );
 // });
 
-const vis = document.querySelectorAll(".header-nav__list-link");
-const drop = document.querySelectorAll(".dropdown-menu");
-
-for (let i = 0; i < vis.length; i++) {
-  vis[i].addEventListener("mouseover", () => {
-    drop[i].classList.add("visible");
-  });
-  vis[i].addEventListener("mouseout", () => {
-    drop[i].classList.remove("visible");
-  });
+function headerMenu() {
+  const hover = document.querySelectorAll(".header-nav__list-item");
+  const dropdown = document.querySelectorAll(".dropdown-menu");
+  for (let i = 0; i < hover.length; i++) {
+    hover[i].addEventListener("mouseover", () => {
+      dropdown[i].classList.add("visible");
+    });
+    hover[i].addEventListener("mouseout", () => {
+      dropdown[i].classList.remove("visible");
+    });
+  }
 }
+headerMenu();
 
