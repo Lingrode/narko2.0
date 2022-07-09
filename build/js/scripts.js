9bc739b167ec0c32d7d8a96135fbec874cd86263
Miron <kolomiietsnazar@gmail.com>
2022-07-09 03:09:30 +0300

    Adaptive Header

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,8 +1,10 @@
 // Custom Scripts
+// Header menu
 function headerMenu() {
   const hover = document.querySelectorAll(".header-nav__list-item");
   const dropdown = document.querySelectorAll(".dropdown-menu");
-  for (let i = 0; i < hover.length; i++) {
+
+  for (let i = 0; i < 4; i++) {
     hover[i].addEventListener("mouseover", () => {
       dropdown[i].classList.add("visible");
     });
@@ -10,6 +12,55 @@ function headerMenu() {
       dropdown[i].classList.remove("visible");
     });
   }
+  for (let i = 0; i < hover.length; i++) {
+    hover[i].addEventListener("mouseover", () => {
+      hover[i].classList.add("hovered");
+    });
+    hover[i].addEventListener("mouseout", () => {
+      hover[i].classList.remove("hovered");
+    });
+  }
 }
 headerMenu();
+
+// Burger menu
+function burgerMenu() {
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".header-menu");
+  burger.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+      menu.classList.add("active");
+      burger.classList.add("active-burger");
+    } else {
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+    }
+  });
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+    }
+  });
+}
+burgerMenu();
+
+// Accordion
+function accordion() {
+  const items = document.querySelectorAll(".accordion__item-trigger");
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      const parent = item.parentNode;
+      if (parent.classList.contains("accordion__item-active")) {
+        parent.classList.remove("accordion__item-active");
+      } else {
+        document
+          .querySelectorAll(".accordion__item")
+          .forEach((child) => child.classList.remove("accordion__item-active"));
+        parent.classList.add("accordion__item-active");
+      }
+    });
+  });
+}
+accordion();
 
